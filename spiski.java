9f2a68b683a369b28a49d002f34afc341a5de00a
lun1pon <150598791+lun1pon@users.noreply.github.com>
2025-09-14 23:52:14 +0300

    Create spiski.java

---
+++ spiski.java
@@ -0,0 +1,5 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+ArrayList<String> fruits = new ArrayList<>(Arrays.asList("apple", "banana", "orange"));
+ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
